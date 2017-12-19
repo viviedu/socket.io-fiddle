7d32ad63db2499a6f3461aea219f9913c56d5883
Jake Barnes <jakerodneybarnes@gmail.com>
2017-12-19 22:48:23 +1100

    Reproduce https://github.com/socketio/socket.io-client/issues/1179

--- client.js
+++ client.js
@@ -6,3 +6,13 @@ socket.on('connect', onConnect);
 function onConnect(){
   console.log('connect ' + socket.id);
 }
+
+socket.on('disconnect', (reason) => console.log('disconnect', reason));
+socket.on('reconnecting', (attemptNumber) => console.log('reconnecting', attemptNumber));
+socket.on('reconnect', (attemptNumber) => console.log('reconnect', attemptNumber));
+socket.on('reconnect_error', (error) => console.log('reconnect_error', error));
+
+setTimeout(() => {
+  console.log('explicitly connecting');
+  socket.connect();
+}, 10 * 1000);
